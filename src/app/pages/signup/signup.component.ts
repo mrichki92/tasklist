f05b0623bc8e17681e00e410563f53c77c692965
richki <mrichki92>
2026-02-16 11:03:59 +0700

    loading bar

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,36 +1,59 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
+import { Route } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   template: `
-    <div class="card">
-      <h2>Create Account ✨</h2>
-      <form [formGroup]="form" (ngSubmit)="submit()">
-        <input formControlName="name" placeholder="Full Name" />
-        <input formControlName="email" placeholder="Email" />
-        <input formControlName="password" type="password" placeholder="Password" />
-        <button>Signup</button>
-      </form>
-      <a class="link" routerLink="/login">Already have account?</a>
-    </div>
+   <div class="card">
+  <h2>Create Account ✨</h2>
+
+  <!-- Loading Bar -->
+  <div class="loading-bar" *ngIf="loading">
+    <div class="loading-progress"></div>
+  </div>
+
+  <form [formGroup]="form" (ngSubmit)="submit()">
+    <input formControlName="name" placeholder="Full Name" />
+    <input formControlName="email" placeholder="Email" />
+    <input formControlName="password" type="password" placeholder="Password" />
+    <button [disabled]="loading">
+      {{ loading ? 'Processing...' : 'Signup' }}
+    </button>
+  </form>
+
+  <a class="link" routerLink="/login">Already have account?</a>
+</div>
+
   `
 })
 export class SignupComponent {
+  router = inject(Router);
   fb = inject(FormBuilder);
   auth = inject(AuthService);
+  loading = false;
 
   form = this.fb.group({ name: '', email: '', password: '' });
 
   submit() {
-    console.log('Form Value:', this.form.value);
+  this.loading = true;
+
+  console.log('Form Value:', this.form.value);
+
+  setTimeout(() => {
+
     this.auth.signup(this.form.value);
     console.log('User saved to localStorage');
+    this.loading = false;
     alert('Signup Success!');
-  }
+    this.router.navigate(['/login']);
+  }, 1200); // simulasi loading 1.2 detik
+}
+
+
 
 }
